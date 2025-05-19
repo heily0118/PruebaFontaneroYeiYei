b0d6a1b79ee6d15b7ca1b312e8b5999bee25549d
Heily Rios <heilyayala005@gmail.com>
2025-05-19 18:29:36 -0500

    first commit

---
+++ src/main/java/autonoma/pruebaunitariafontaneroyeiyei/PruebaUnitariaFontaneroYeiYei.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package autonoma.pruebaunitariafontaneroyeiyei;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class PruebaUnitariaFontaneroYeiYei {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
